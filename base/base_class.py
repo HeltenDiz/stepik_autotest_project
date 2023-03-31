b698ce9e48b7a8bd2d1579a9c3e8fb0fcb17f60d
da.boyarincev <da.boyarincev@tensor.ru>
2023-03-31 17:56:02 +0300

    login page update

--- base/base_class.py
+++ base/base_class.py
@@ -1,14 +1,18 @@
 from datetime import datetime
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
+import selenium.common.exceptions as selenium_ex
 
 
 class Base:
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self, browser):
+        self.browser = browser
 
     def get_current_url(self):
         """Method get current url"""
-        get_url = self.driver.current_url
+        get_url = self.browser.current_url
         print("Current url " + get_url)
 
     def assert_word(self, word, result):
@@ -17,15 +21,35 @@ class Base:
         assert value_word == result
         print("Correct value word")
 
+    def check_open(self, locator):
+        element = WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(locator))
+        element.is_displayed()
+
+    def is_element_present(self, how, what):
+        try:
+            element = self.browser.find_element(how, what)
+            element.is_displayed()
+        except selenium_ex.NoSuchElementException:
+            return False
+        return True
+
+    def assert_exist_element(self, how, what, msg):
+        assert self.is_element_present(how, what), msg
+
+    def get_clickable_element(self, locator):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(locator))
+
     def make_screenshot(self):
         """Method make screenshot"""
         now_date = datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
         name_screenshot = 'screenshot_' + now_date + '.png'
-        self.driver.save_screenshot(
+        self.browser.save_screenshot(
             'C:\\Users\\da.boyarincev\\Downloads\\PyProjects\\main_project\\screen' + name_screenshot)
 
     def assert_url(self, result):
         """Method assert url"""
-        get_url = self.driver.current_url
+        get_url = self.browser.current_url
         assert get_url == result
         print("Correct value url")
+
+
--- pages/login_page.py
+++ pages/login_page.py
@@ -2,36 +2,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from stepik_autotest_project.base.base_class import Base
+from base.base_class import Base
 
 
 class LoginPage(Base):
-    base_url = 'https://www.saucedemo.com/'
+    base_url = 'https://www.computeruniverse.net/'
 
-    def __init__(self, driver):
-        super().__init__(driver)
-        self.driver = driver
+    def __init__(self, browser):
+        super().__init__(browser)
+        self.browser = browser
 
     # Locators
-    username = "//input[@id='user-name']"
-    password = "#password"
-    button_login = "//input[@value='Login']"
-    main_word = "//span[@class='title']"
-    result = 'PRODUCTS'
+    login_popup = (By.CLASS_NAME, "[class='c-LoginPopOver__content']")
+    username = (By.ID, "[id='login-form_id']")
+    password = (By.ID, "[id='login-form_password']")
+    button_login = (By.TAG_NAME, "button.at__login__login")
+    customer_log = (By.CSS_SELECTOR, "[.customer--logged-in .icon-hook]")
 
     # Getters
 
     def get_user_name(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.username)))
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(self.username))
 
     def get_user_password(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.password)))
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(self.password))
 
     def get_login_button(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_login)))
-
-    def get_main_word(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.main_word)))
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(self.button_login))
 
     # Actions
 
@@ -51,10 +48,9 @@ class LoginPage(Base):
 
     def authorization(self, user_name, user_password):
         """Autorization"""
-        self.driver.get(self.base_url)
-        self.driver.maximize_window()
-        self.get_current_url()
+        self.check_open(self.login_popup)
         self.input_user_name(user_name)
         self.input_user_password(user_password)
         self.click_login_button()
-        #self.assert_word(self.get_main_word, self.result)
+        self.assert_exist_element(*self.customer_log, msg='Customer is not authorized')
+        self.get_current_url()
