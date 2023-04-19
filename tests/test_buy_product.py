2acae8f20937f258890fee5e4cff24b95322353f
da.boyarincev <da.boyarincev@tensor.ru>
2023-04-19 22:26:52 +0300

    tests

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -8,8 +8,8 @@ from pages.product_selection_page import ProductSelectionPage
 
 @pytest.mark.usefixtures("browser")
 class TestBuyProduct:
-    login_name = ""
-    login_password = ""
+    login_name = "workdiz9@gmail.com"
+    login_password = "test123"
     top_menu = ProductSelectionPage.computer_parts
     submenu = ProductSelectionPage.video_card
     phone_num = "89787425288"
